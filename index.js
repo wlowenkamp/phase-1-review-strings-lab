6c0efe3f051f9e3ac753497850dd97820e5cdb39
Will Lowenkamp <lowenkamp97@gmail.com>
2023-05-22 11:26:27 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+const currentUser = "Will Lowenkamp";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase ();
+    console.log(excitedWelcomeMessage)
+const shortGreeting = "Welcome, " + currentUser.slice(0,1) + "!";
+    console.log(shortGreeting)
